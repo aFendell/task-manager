368534cd80ced3749eb8ba68c40a6dea602d8530
Assaf Fendell <fendell.assaf@gmail.com>
2023-12-06 13:47:33 +0200

    feat: location based header nav

--- apps/client/src/components/layout/Header.tsx
+++ apps/client/src/components/layout/Header.tsx
@@ -3,13 +3,31 @@ import * as React from 'react';
 import { Button } from '@/components/ui/Button';
 import TaskForm from '@/screens/app/Tasks/TaskForm';
 import ToggleMode from '@/components/ui/ToggleMode';
+import { NavLink, useLocation } from 'react-router-dom';
+import Path from '@/routes/paths';
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   const [isTaskForm, setIsTaskForm] = React.useState(false);
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container flex h-16 w-full items-center justify-between'>
-        <Button onClick={() => setIsTaskForm(true)}>Create Task</Button>
+        {pathname.includes(Path.Tasks) ? (
+          <Button onClick={() => setIsTaskForm(true)}>Create Task</Button>
+        ) : pathname.includes(Path.SignUp) ? (
+          <Button>
+            <NavLink replace to={`/${Path.Login}`}>
+              Login
+            </NavLink>
+          </Button>
+        ) : (
+          <Button>
+            <NavLink replace to={`/${Path.SignUp}`}>
+              Sign Up
+            </NavLink>
+          </Button>
+        )}
 
         <ToggleMode />
       </div>
