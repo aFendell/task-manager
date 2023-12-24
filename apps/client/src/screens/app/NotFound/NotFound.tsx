ab6e4abc42953b8a411fe3dd43a450bdfb113612
Assaf Fendell <fendell.assaf@gmail.com>
2023-12-24 17:37:08 +0200

    fix: NotFound button margin

--- apps/client/src/screens/app/NotFound/NotFound.tsx
+++ apps/client/src/screens/app/NotFound/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       <h1 className='text-2xl font-semibold tracking-tight'>
         The page you're looking for was Not Found
       </h1>
-      <Button>
+      <Button className='mt-5'>
         <NavLink to='/'>Go back</NavLink>
       </Button>
     </section>
