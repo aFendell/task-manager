e2954f20928f816e828d6169c5bc8347425bcb0a
Assaf Fendell <fendell.assaf@gmail.com>
2023-12-24 17:38:04 +0200

    fix: not found lowercase

--- apps/client/src/screens/app/NotFound/NotFound.tsx
+++ apps/client/src/screens/app/NotFound/NotFound.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <section>
       <h1 className='text-2xl font-semibold tracking-tight'>
-        The page you're looking for was Not Found
+        The page you're looking for was not found
       </h1>
       <Button className='mt-5'>
         <NavLink to='/'>Go back</NavLink>
